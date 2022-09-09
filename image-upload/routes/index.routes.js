67586f7ad1d19b24e7d33da50f5b4982aa0ace3f
Jan Redmann <janredmann1@gmail.com>
2022-09-09 10:48:32 +0200

    add image upload

--- image-upload/routes/index.routes.js
+++ image-upload/routes/index.routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Movie = require('../models/Movie')
+const uploader = require('../config/cloudinary')
 
 /* GET home page */
 router.get("/", (req, res, next) => {
@@ -16,8 +17,20 @@ router.get('/movie/add', (req, res, next) => {
   res.render('movie-add')
 });
 
-router.post('/movies', (req, res, next) => {
+// the argument for the uploader is the value of the name attribute
+// in the form
+router.post('/movies', uploader.single('poster'), (req, res, next) => {
+  // this is where express / multer adds the info about the uploaded file
+  console.log(req.file)
   const { title, description } = req.body
+  const imgName = req.file.originalname
+  const imgPath = req.file.path
+  Movie.create({ title, description, imgName, imgPath })
+    .then(movie => {
+      console.log(movie)
+      res.redirect('/')
+    })
+    .catch(err => next(err))
 });
 
 router.get('/movie/delete/:id', (req, res, next) => {
